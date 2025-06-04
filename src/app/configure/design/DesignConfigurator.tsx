5ce876c259d1c70ad117792ebd9e9fd20013f485
dynamix12 <kalin97@ymail.com>
2025-06-04 03:28:25 +0300

    Created the designconfigurator

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,12 @@
+interface DesignConfiguratorProps {
+  configId: string;
+  imageUrl: string;
+  imageDimensions: { width: number; height: number };
+}
+
+const DesignConfigurator = ({
+  configId,
+  imageUrl,
+  imageDimensions,
+}: DesignConfiguratorProps) => {};
+export default DesignConfigurator;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,19 +1,31 @@
+import { db } from "@/db";
 import { notFound } from "next/navigation";
 
 interface PageProps {
-  searchParams: {
+  searchParams: Promise<{
     [key: string]: string | string[] | undefined;
-  };
+  }>;
 }
 
 const Page = async ({ searchParams }: PageProps) => {
-  const { id } = searchParams;
+  const params = await searchParams;
+  const { id } = params;
 
   if (!id || typeof id !== "string") {
     return notFound();
   }
 
-  return <p>{id}</p>;
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return <DesignConfigurator />;
 };
 
 export default Page;
